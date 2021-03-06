deee33a7f56dacab3c70acd179f9c19390ed1df7
szymonkat <szymonkat@gmail.com>
2021-03-06 23:30:44 +0100

    Update
    Customer removal

--- src/main/java/com/app/account/config/ServiceConfig.java
+++ src/main/java/com/app/account/config/ServiceConfig.java
@@ -1,21 +1,13 @@
 package com.app.account.config;
 
 import com.app.account.repository.AccountRepository;
-import com.app.account.repository.CustomerRepository;
 import com.app.account.service.implementations.AccountServiceImpl;
-import com.app.account.service.implementations.CustomerServiceImpl;
 import com.app.account.service.interfaces.AccountService;
-import com.app.account.service.interfaces.CustomerService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ServiceConfig {
-
-    @Bean
-    protected CustomerService customerService(CustomerRepository customerRepository) {
-        return new CustomerServiceImpl(customerRepository);
-    }
 
     @Bean
     protected AccountService accountService(AccountRepository accountRepository) {
