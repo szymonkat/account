460e016bf087bb8a5c08d537e7ea7de2751ceba6
szymonkat <szymonkat@gmail.com>
2021-03-06 21:21:28 +0100

    BSA
    boot starter actuator

--- src/main/java/com/app/account/controller/CustomerController.java
+++ src/main/java/com/app/account/controller/CustomerController.java
@@ -4,21 +4,35 @@ import com.app.account.dto.CustomerDto;
 import com.app.account.mapper.CustomerMapper;
 import com.app.account.service.interfaces.CustomerService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
+@Slf4j
+@RefreshScope
 @RestController
 @RequestMapping("/v1/accounts")
 @RequiredArgsConstructor
 public class CustomerController {
+
+    @Value("${application.allow-get-accounts}")
+    private boolean allowGetAccounts;
 
     private final CustomerMapper customerMapper;
     private final CustomerService customerService;
 
     @GetMapping
     public CustomerDto getAccountsByCustomerId(@RequestParam Long customerId)  {
+        if(!allowGetAccounts) {
+            log.info("Getting accounts is disabled");
+            throw new ResponseStatusException(HttpStatus.SERVICE_UNAVAILABLE, "Getting accounts is disabled");
+        }
         return customerMapper.mapToCustomerDto(customerService.getCustomerById(customerId));
     }
 }
--- src/main/java/com/app/account/service/implementations/CustomerServiceImpl.java
+++ src/main/java/com/app/account/service/implementations/CustomerServiceImpl.java
@@ -1,28 +1,33 @@
 package com.app.account.service.implementations;
 
+import com.app.account.domain.Account;
+import com.app.account.domain.Currency;
 import com.app.account.domain.Customer;
 import com.app.account.exceptions.NotFoundException;
 import com.app.account.repository.CustomerRepository;
 import com.app.account.service.interfaces.CustomerService;
 import lombok.RequiredArgsConstructor;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
 @RequiredArgsConstructor
 public class CustomerServiceImpl implements CustomerService {
 
     private final CustomerRepository customerRepository;
-    //private final CustomerService customerService;
 
     @Override
     public Customer getCustomerById(Long customerId) throws NotFoundException {
         // Test response
-        /*Account account = new Account(1421L, "72249000059957936727967706", Currency.PLN, 6525.11F);
+        Account account = new Account(1421L, "72249000059957936727967706", Currency.PLN, BigDecimal.valueOf(652543243.213));
         List<Account> accountList = new ArrayList<>();
         accountList.add(account);
         Customer customer = new Customer(123456L, accountList);
 
-        return customerId == 123456 ? customer : null;*/
-
+        return customerId == 123456 ? new Customer(123456L, new ArrayList<>(accountList)) : null;
+/*
         return customerRepository.findById(customerId).orElseThrow(
-                () -> new NotFoundException("Customer with id: " + customerId + " does not exist"));
+                () -> new NotFoundException("Customer with id: " + customerId + " does not exist"));*/
     }
 }
